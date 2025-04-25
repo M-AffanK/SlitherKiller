f0ecc45b4de66feec06663058fd8f11903f7493a
MuhammadAffan-114 <bscs23114@itu.edu.pk>
2025-04-25 06:51:10 +0500

    Add: signup_screen

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:slitherkiller/Routes/app_routes.dart';
+import 'package:slitherkiller/utils/app_images.dart';
+import 'package:slitherkiller/utils/app_colors.dart';
+import 'package:slitherkiller/utils/app_text.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(AppImages.mainBg, fit: BoxFit.cover),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Positioned(
+                  top: -10.h,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontFamily: 'JustAnotherHand',
+                        fontSize: 0.16.sh,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 160.h,
+                  left: 30.w,
+                  right: 30.w,
+
+                  child: const Text(
+                    'Username:',
+                    style: TextStyle(
+                      fontFamily: 'JustAnotherHand',
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'John-Cloe_1234',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 160.h,
+                  left: 30.w,
+                  right: 30.w,
+                  child: const Text(
+                    'Name:',
+                    style: TextStyle(
+                      fontFamily: 'JustAnotherHand',
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'John Cloe',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                Positioned(
+                  top: 160.h,
+                  left: 30.w,
+                  right: 30.w,
+                  child: const Text(
+                    'Email:',
+                    style: TextStyle(
+                      fontFamily: 'JustAnotherHand',
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'johncloe@gmail.com',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 160.h,
+                  left: 30.w,
+                  right: 30.w,
+                  child: const Text(
+                    'Password:',
+                    style: TextStyle(
+                      fontFamily: 'JustAnotherHand',
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'abcd1234!@#\$',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.lightGreen,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                  ),
+                  onPressed: () {
+                    // TODO: Implement login functionality
+                    Get.offNamed(AppRoutes.setting);
+                  },
+                  child: const Text(
+                    'Done',
+                    style: TextStyle(
+                      fontFamily: 'JustAnotherHand',
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
